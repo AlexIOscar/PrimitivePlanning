0eee6b176e85957202fe9f8a1e8854113e5557fd
AlexIOscar <zobr85@list.ru>
2022-08-25 08:30:02 +0600

    Initial commit

---
+++ src/debug/Starter.java
@@ -0,0 +1,45 @@
+package debug;
+
+import domain.Timeline;
+
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+
+public class Starter {
+    public static void main(String[] args) {
+        Timeline tl = new Timeline();
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(2022, Calendar.AUGUST, 25, 12, 0, 0);
+        Date d = cal.getTime();
+
+        tl.engageTime(d, 3600);
+        System.out.println("before");
+        System.out.println(tl);
+
+        System.out.println("after 1");
+        tl.findTime(new Date(d.getTime() - 1500), 600);
+        System.out.println(tl);
+
+        System.out.println("after 2");
+        tl.findTime(new Date(d.getTime() - 1500), 600);
+        System.out.println(tl);
+
+        System.out.println("размещаем промежуток 5 минут");
+        cal.set(2022, Calendar.AUGUST, 25, 10, 35, 0);
+        Date d2 = cal.getTime();
+        tl.engageTime(d2, 300);
+        System.out.println(tl);
+
+        System.out.println("after 3");
+        tl.findTime(new Date(d.getTime() - 1500), 660);
+        System.out.println(tl);
+
+        System.out.println("after 4");
+        tl.findTime(new Date(d.getTime() - 1500), 240);
+        System.out.println(tl);
+
+
+    }
+}
---
+++ src/domain/Timeline.java
@@ -0,0 +1,137 @@
+package domain;
+
+import java.util.*;
+
+//класс, предназначенный для хранения данных доступности ресурса
+public class Timeline {
+
+    //через простой список
+    private final LinkedList<TimeGap> busyTimes;
+
+    public Timeline() {
+        this.busyTimes = new LinkedList<>();
+    }
+
+    //занять время на таймлайне (хардово, без любых проверок). Последующая сортировка плоха для перформанса, зато
+    // гарантирует получение упорядоченного листа после вызова
+    public void engageTime(Date opEnding, double duration) {
+        Date opStart = new Date((long) (opEnding.getTime() - duration * 1000));
+        busyTimes.add(new TimeGap(opStart, opEnding));
+        Collections.sort(busyTimes);
+    }
+
+    //сохренение верной сортировки - на стороне кода-клиента
+    public void engageByIndex(Date opEnding, double duration, int index) {
+        Date opStart = new Date((long) (opEnding.getTime() - duration * 1000));
+        busyTimes.add(index, new TimeGap(opStart, opEnding));
+    }
+
+    //сохренение верной сортировки - на стороне кода-клиента
+    public void engageLeft(Date opEnding, double duration) {
+        Date opStart = new Date((long) (opEnding.getTime() - duration * 1000));
+        busyTimes.addFirst(new TimeGap(opStart, opEnding));
+    }
+
+    //найти свободное время на таймлайне и занять его
+    public void findTime(Date notLater, double duration) {
+        //TimeGap collide = checkCollide(notLater);
+        int closestLeft = closestIndexLeft(notLater);
+
+        //если точка левее всех отрезков, то просто занимаем самое "левое" время. Сортировка сохраняется.
+        if (closestLeft == -1) {
+            engageLeft(notLater, duration);
+            return;
+        }
+
+        //получаем отрезок, начало которого ближайшее слева
+        TimeGap closest = busyTimes.get(closestLeft);
+        //возможны две ситуации - тестируемая точка лежит внутри отрезка (true), или правее (false). Проверяем:
+        //если правее, то чекаем что "места" хватит на новый отрезок, если хватает, то записываем отрезок по индексу
+        // closestLeft + 1 и выходим
+        if (!checkCollide(notLater, closest)) {
+            if (notLater.getTime() - closest.gapEn.getTime() >= duration * 1000) {
+                engageByIndex(notLater, duration, closestLeft + 1);
+                return;
+            }
+        }
+        //если тестируемая точка лежит внутри closest, то необходимо проверить все пустые места поочередно влево, и
+        // по возможности занять ближайшее
+        for (int i = closestLeft; i > 0; i--) {
+            closest = busyTimes.get(i);
+            if (isFitBetween(busyTimes.get(i - 1), closest, duration)) {
+                engageByIndex(closest.gapSt, duration, i);
+                return;
+            }
+        }
+        //если места так и не нашлось, занимаем место слева вплотную к нулевому отрезку (который станет после этого
+        // первым)
+        engageLeft(busyTimes.get(0).gapSt, duration);
+    }
+
+    //проверяем уместится ли операция между отрезками
+    private boolean isFitBetween(TimeGap tg1, TimeGap tg2, double duration) {
+        return tg2.gapSt.getTime() - tg1.gapEn.getTime() >= duration * 1000;
+    }
+
+    //проверяем момент времени на "столкновение" с любым из занятых промежутков. если занят, то возвращается
+    // промежуток, с которым произошла коллизия, если свободен - null.
+    private TimeGap checkAnyCollide(Date moment) {
+        for (int i = busyTimes.size() - 1; i >= 0; i--) {
+            TimeGap gap = busyTimes.get(i);
+            if (moment.before(gap.gapEn) && moment.after(gap.gapSt)) {
+                return gap;
+            }
+        }
+        return null;
+    }
+
+    //true if collide
+    private boolean checkCollide(Date moment, TimeGap tg) {
+        return moment.after(tg.gapSt) && moment.before(tg.gapEn);
+    }
+
+    //выяснить индекс отрезка, стартовая точка которого является ближайшей слева к moment
+    private int closestIndexLeft(Date moment) {
+        for (int i = busyTimes.size() - 1; i >= 0; i--) {
+            if (busyTimes.get(i).gapSt.before(moment)) {
+                return i;
+            }
+        }
+        //слева нет отрезков
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        return "Timeline{" +
+                "busyTimes=" + busyTimes +
+                '}';
+    }
+
+    //промежуток времени на таймлайне
+    static class TimeGap implements Comparable<TimeGap> {
+        Date gapSt;
+        Date gapEn;
+
+        public TimeGap(Date gapSt, Date gapEn) {
+            this.gapSt = gapSt;
+            this.gapEn = gapEn;
+        }
+
+        @Override
+        public String toString() {
+            return "TimeGap {" +
+                    "gapSt=" + gapSt +
+                    ", gapEn=" + gapEn +
+                    '}';
+        }
+
+        @Override
+        //сравнение двух временных отрезков
+        public int compareTo(TimeGap o) {
+            if (this.gapSt.before(o.gapSt)) {
+                return -1;
+            } else return 1;
+        }
+    }
+}
